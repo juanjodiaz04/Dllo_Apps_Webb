0cbe4ef2707c2dbbd9b63b0c7541ae96f24274ce
juanjodiaz04 <juanj.diaz1@udea.edu.co>
2025-01-21 11:24:00 -0500

    Adicion JS

---
+++ Parcial1/scripts/script.js
@@ -0,0 +1,12 @@
+class car {
+    constructor(brand, model, year, color, price, km) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.km = km;
+    }
+}
+
+const car1 = new car("Toyota", "Corolla", 2019, "Blanco", 200000, 10000);
